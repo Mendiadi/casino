28fc3ac55a4533b1d053792c632b6f17293d2b59
Adi Mendel <adim@sela.co.il>
2023-10-03 01:59:09 +0300

    gui2.0

--- screens.py
+++ screens.py
@@ -2,105 +2,119 @@ import pygame
 import pygame_gui
 import random
 
-# Initialize pygame
-pygame.init()
-
-# Define screen dimensions
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-# Create the Pygame screen
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Casino Lobby")
-
-# Initialize pygame_gui
-# pygame_gui.init()
-
-# Create a manager for the GUI elements
-gui_manager = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-# Define colors
-WHITE = (255, 255, 255)
-BACKGROUND_COLOR = (0, 128, 0)
-
-# Create background surface
-background = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-background.fill(BACKGROUND_COLOR)
-
-# Create a font for the headline
-headline_font = pygame.font.Font(None, 72)
-headline_text = headline_font.render("Casino", True, WHITE)
-
-# Load images for casino games
-slot_machine_image = pygame.image.load(r"C:\Users\adim\PycharmProjects\gui_project\world\assets\money.png")  # Replace with your image
-roulette_table_image = pygame.image.load(r"C:\Users\adim\PycharmProjects\gui_project\world\assets\money.png")  # Replace with your image
-
-# Create buttons for casino games
-slot_machine_button = pygame_gui.elements.UIButton(
-    relative_rect=pygame.Rect(100, 100, 200, 100),
-    text="Play PVP",
-    manager=gui_manager,
-)
-roulette_table_button = pygame_gui.elements.UIButton(
-    relative_rect=pygame.Rect(400, 100, 200, 100),
-    text="About us",
-    manager=gui_manager,
-)
-
-# Animation variables
-game_objects = []
 
 
-def create_game_object(image):
-    x = random.randint(50, SCREEN_WIDTH - 50)
-    y = random.randint(-200, -50)
-    speed = random.randint(1, 3)
-    return {"image": image, "rect": pygame.Rect(x, y, 100, 100), "speed": speed}
+class CasinoLobby:
+
+    def create_game_object(self,image):
+        x = random.randint(50, self.SCREEN_WIDTH - 50)
+        y = random.randint(-200, -50)
+        speed = random.randint(1, 3)
+        return {"image": image, "rect": pygame.Rect(x, y, 100, 100), "speed": speed}
+    def __init__(self):
+        pygame.init()
+
+        # Define screen dimensions
+        self.SCREEN_WIDTH = 800
+        self.SCREEN_HEIGHT = 600
+
+        # Create the Pygame screen
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        pygame.display.set_caption("Casino Lobby")
+
+        # Create a manager for the GUI elements
+        self.gui_manager = pygame_gui.UIManager((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+
+        # Define colors
+        self.WHITE = (255, 255, 255)
+        self.BACKGROUND_COLOR = (0, 128, 0)
+
+        # Create background surface
+        self.background = pygame.Surface((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.background.fill(self.BACKGROUND_COLOR)
+
+        # Create a font for the headline
+        self.headline_font = pygame.font.Font(None, 72)
+        self.headline_text = self.headline_font.render("Casino", True, self.WHITE)
+
+        # Load images for casino games
+        self.slot_machine_image = pygame.image.load(
+            r"C:\Users\adim\PycharmProjects\gui_project\world\assets\money.png")  # Replace with your image
+        self.roulette_table_image = pygame.image.load(
+            r"C:\Users\adim\PycharmProjects\gui_project\world\assets\money.png")  # Replace with your image
+
+        # Create buttons for casino games
+        self.slot_machine_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect(100, 100, 200, 100),
+            text="Play PVP",
+            manager=self.gui_manager,
+        )
+        self.roulette_table_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect(400, 100, 200, 100),
+            text="About us",
+            manager=self.gui_manager,
+        )
+
+        # Animation variables
+        self.game_objects = []
+
+        def create_game_object(image):
+            x = random.randint(50, self.SCREEN_WIDTH - 50)
+            y = random.randint(-200, -50)
+            speed = random.randint(1, 3)
+            return {"image": image, "rect": pygame.Rect(x, y, 100, 100), "speed": speed}
+
+        self.game_objects.append(create_game_object(self.slot_machine_image))
+        self.game_objects.append(create_game_object(self.roulette_table_image))
+
+        # Headline animation variables
+        self.headline_x = 350
+        self.headline_y = 20
+        self.headline_speed = 1
+
+        # Main loop variables
+        self.running = True
+        self.clock = pygame.time.Clock()
+    def draw(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+            # Process GUI events
+            self.gui_manager.process_events(event)
+
+        # Update animations
+        for game_object in self.game_objects:
+            game_object["rect"].move_ip(0, game_object["speed"])
+            if game_object["rect"].top > self.SCREEN_HEIGHT:
+                self.game_objects.remove(game_object)
+                self.game_objects.append(self.create_game_object(game_object["image"]))
+
+        self.headline_x += self.headline_speed
+        if self.headline_x > self.SCREEN_WIDTH:
+            self.headline_x = -self.headline_text.get_width()
+
+        self.gui_manager.update(self.clock.tick(60) / 1000.0)
+
+        self.screen.blit(self.background, (0, 0))
+
+        for game_object in self.game_objects:
+            self.screen.blit(game_object["image"], game_object["rect"])
+
+        self.screen.blit(self.headline_text, (self.headline_x, self.headline_y))
+
+        self.gui_manager.draw_ui(self.screen)
+
+        pygame.display.flip()
+    def mainloop(self):
+        while self.running:
+            self.draw()
+
+        # Quit Pygame
+        pygame.quit()
 
 
-game_objects.append(create_game_object(slot_machine_image))
-game_objects.append(create_game_object(roulette_table_image))
-
-# Headline animation variables
-headline_x = 350
-headline_y = 20
-headline_speed = 1
-
-# Main loop
-running = True
-clock = pygame.time.Clock()
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-        # Process GUI events
-        gui_manager.process_events(event)
-
-    # Update animations
-    for game_object in game_objects:
-        game_object["rect"].move_ip(0, game_object["speed"])
-        if game_object["rect"].top > SCREEN_HEIGHT:
-            game_objects.remove(game_object)
-            game_objects.append(create_game_object(game_object["image"]))
-
-    headline_x += headline_speed
-    if headline_x > SCREEN_WIDTH:
-        headline_x = -headline_text.get_width()
-
-    gui_manager.update(clock.tick(60) / 1000.0)
-
-    screen.blit(background, (0, 0))
-
-    for game_object in game_objects:
-        screen.blit(game_object["image"], game_object["rect"])
-
-    screen.blit(headline_text, (headline_x, headline_y))
-
-    gui_manager.draw_ui(screen)
-
-    pygame.display.flip()
-
-# Quit Pygame
-pygame.quit()
+# Instantiate and run the CasinoLobby
+if __name__ == "__main__":
+    casino_lobby = CasinoLobby()
+    casino_lobby.mainloop()
--- welcome_screen.py
+++ welcome_screen.py
@@ -1,128 +1,138 @@
-import threading
-
 import pygame
 import pygame_gui
 import requests
+import threading
 
 import gui_
 import routes
 
-# Initialize Pygame
-pygame.init()
 
-# Constants
-WINDOW_SIZE = (400, 400)
-HEADLINE_TEXT = "Welcome"
-USER_ID_PLACEHOLDER = "Enter User ID"
+class LoginScreen:
+    def __init__(self):
+        pygame.init()
 
-# Create the Pygame window
-pygame.display.set_caption("Login Screen")
-window_surface = pygame.display.set_mode(WINDOW_SIZE)
+        # Constants
+        self.WINDOW_SIZE = (400, 400)
+        self.HEADLINE_TEXT = "Welcome"
+        self.USER_ID_PLACEHOLDER = "Enter User ID"
 
-# Create a GUI manager
-gui_manager = pygame_gui.UIManager(WINDOW_SIZE)
+        # Create the Pygame window
+        pygame.display.set_caption("Login Screen")
+        self.window_surface = pygame.display.set_mode(self.WINDOW_SIZE)
 
-# Create a background circle
-circle_radius = 10
-circle_color = (255, 0, 0)
-circle_x = -circle_radius
+        # Create a GUI manager
+        self.gui_manager = pygame_gui.UIManager(self.WINDOW_SIZE)
 
-# Create GUI components
-headline_label = pygame_gui.elements.UILabel(
-    relative_rect=pygame.Rect((50,10), (300, 70)),
-    text=HEADLINE_TEXT,
-    manager=gui_manager,
-    object_id="headline_label",
-)
-  # Increase the font size
+        # Create a background circle
+        self.circle_radius = 10
+        self.circle_color = (255, 0, 0)
+        self.circle_x = -self.circle_radius
 
-user_id_entry = pygame_gui.elements.UITextEntryLine(
-    relative_rect=pygame.Rect((50, 100), (300, 40)),
-    manager=gui_manager,
-    object_id="user_id_entry",
-)
-user_id_entry.set_text(USER_ID_PLACEHOLDER)
+        # Create GUI components
+        self.headline_label = pygame_gui.elements.UILabel(
+            relative_rect=pygame.Rect((50, 10), (300, 70)),
+            text=self.HEADLINE_TEXT,
+            manager=self.gui_manager,
+            object_id="headline_label",
+        )
 
-login_button = pygame_gui.elements.UIButton(
-    relative_rect=pygame.Rect((50, 160), (100, 40)),
-    text="Login",
-    manager=gui_manager,
-)
+        self.user_id_entry = pygame_gui.elements.UITextEntryLine(
+            relative_rect=pygame.Rect((50, 100), (300, 40)),
+            manager=self.gui_manager,
+            object_id="user_id_entry",
+        )
+        self.user_id_entry.set_text(self.USER_ID_PLACEHOLDER)
 
-register_button = pygame_gui.elements.UIButton(
-    relative_rect=pygame.Rect((200, 160), (100, 40)),
-    text="Register",
-    manager=gui_manager,
-)
+        self.login_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((50, 160), (100, 40)),
+            text="Login",
+            manager=self.gui_manager,
+        )
 
-# Variable to track if placeholder text is shown
-placeholder_shown = True
+        self.register_button = pygame_gui.elements.UIButton(
+            relative_rect=pygame.Rect((200, 160), (100, 40)),
+            text="Register",
+            manager=self.gui_manager,
+        )
 
-# Main loop
-clock = pygame.time.Clock()
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.USEREVENT:
-            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-                if event.ui_element == login_button:
-                    r = requests.post(
-                        f"{routes.Routes.prefix}{routes.Routes.host_url}:{routes.Routes.service_session_auth_port}/{routes.Routes.service_session_auth_login}",
-                        json={"user_id": user_id_entry.get_text()})
-                    print(r, r.text)
-                    if r:
-                        pygame.quit()
-                        threading.Thread(target=gui_.run,args=(user_id_entry.get_text(),)).start()
+        # Variable to track if placeholder text is shown
+        self.placeholder_shown = True
 
-                        running = False
-                        break
-                elif event.ui_element == register_button:
-                    def register():
+        # Main loop variables
+        self.clock = pygame.time.Clock()
+        self.running = True
 
+    def draw(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == self.login_button:
                         r = requests.post(
-                            f"{routes.Routes.prefix}{routes.Routes.host_url}:{routes.Routes.service_session_auth_port}/{routes.Routes.service_session_auth_register}",
-                            json={"user_id": user_id_entry.get_text()})
+                            f"{routes.Routes.prefix}{routes.Routes.host_url}:{routes.Routes.service_session_auth_port}/{routes.Routes.service_session_auth_login}",
+                            json={"user_id": self.user_id_entry.get_text()})
                         print(r, r.text)
-                        if r.ok:
-                            print("register sucess")
-                        else:
-                            print(r.text)
-                    threading.Thread(target=register, daemon=True, name="register").start()
+                        if r:
+                            pygame.quit()
+                            threading.Thread(target=gui_.run, args=(self.user_id_entry.get_text(),)).start()
+                            self.running = False
+                            break
+                    elif event.ui_element == self.register_button:
+                        def register():
+                            r = requests.post(
+                                f"{routes.Routes.prefix}{routes.Routes.host_url}:{routes.Routes.service_session_auth_port}/{routes.Routes.service_session_auth_register}",
+                                json={"user_id": self.user_id_entry.get_text()})
+                            print(r, r.text)
+                            if r.ok:
+                                print("register success")
+                            else:
+                                print(r.text)
 
-        gui_manager.process_events(event)
+                        threading.Thread(target=register, daemon=True, name="register").start()
 
-        # Check if the text entry is focused
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if user_id_entry.rect.collidepoint(event.pos):
-                if placeholder_shown:
-                    user_id_entry.set_text("")
-                    placeholder_shown = False
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if not user_id_entry.rect.collidepoint(event.pos) and not user_id_entry.get_text():
-                user_id_entry.set_text(USER_ID_PLACEHOLDER)
-                placeholder_shown = True
+            self.gui_manager.process_events(event)
 
-    # Clear the screen
+            # Check if the text entry is focused
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.user_id_entry.rect.collidepoint(event.pos):
+                    if self.placeholder_shown:
+                        self.user_id_entry.set_text("")
+                        self.placeholder_shown = False
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if not self.user_id_entry.rect.collidepoint(event.pos) and not self.user_id_entry.get_text():
+                    self.user_id_entry.set_text(self.USER_ID_PLACEHOLDER)
+                    self.placeholder_shown = True
+
+        # Clear the screen
+        self.window_surface.fill((0, 0, 0))
+
+        # Move the circle behind the screen
+        self.circle_x += 2
+        if self.circle_x > self.WINDOW_SIZE[0] + self.circle_radius:
+            self.circle_x = -self.circle_radius
+        pygame.draw.circle(self.window_surface, self.circle_color, (self.circle_x, 200), self.circle_radius)
+
+        # Update the GUI manager
+        self.gui_manager.update(1 / 60.0)
+
+        # Draw the GUI
+        self.gui_manager.draw_ui(self.window_surface)
+
+        # Update the display
+        pygame.display.update()
+
+        self.clock.tick(60)
+
+    def mainloop(self):
+        while self.running:
+            self.draw()
+
+        # Quit Pygame
+        pygame.quit()
 
 
-    # Move the circle behind the screen
-    circle_x += 2
-    if circle_x > WINDOW_SIZE[0] + circle_radius:
-        circle_x = -circle_radius
-    pygame.draw.circle(window_surface, circle_color, (circle_x, 200), circle_radius)
-
-    # Update the GUI manager
-    gui_manager.update(1 / 60.0)
-
-    # Draw the GUI
-    gui_manager.draw_ui(window_surface)
-
-    # Update the display
-    pygame.display.update()
-
-    clock.tick(60)
-
-# Quit Pygame
-pygame.quit()
+# Instantiate and run the LoginScreen
+if __name__ == "__main__":
+    login_screen = LoginScreen()
+    login_screen.mainloop()
